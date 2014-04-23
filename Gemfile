e5ba7d7085429f8ad3b783c0e1a7aa8e79999c15
vincent rotter & Liz Tom <student@epicodus.com>
2014-04-23 09:13:52 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,36 @@
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'jquery-turbolinks'
+gem 'bcrypt'
+gem 'paperclip', :git => "git://github.com/thoughtbot/paperclip.git"
+gem 'bootstrap-sass'
+gem 'devise'
+gem 'cancancan'
+gem 'rest-client'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+  gem 'letter_opener'
+end
+
+group :test, :development do
+  gem 'dotenv-rails'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'launchy'
+  gem 'poltergeist'
+  gem 'pry'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'vcr'
+  gem 'webmock'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CashForCharity::Application.config.secret_key_base = '563635266313c4ba79b085e33e8442fc23a0da0767b18920823d1567ad6e24b6cb51d34a33c56a58d12a3e73c5fda55ced61b98163e714619125ea1312f445ea'
